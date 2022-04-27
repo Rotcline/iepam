a14e116393f0ba6970bad7750952d27295fc7733
omarcr02 <a00829941@itesm.mx>
2022-04-27 13:27:11 -0500

    maxUnityTest

--- src/components/UnityFrame/index.tsx
+++ src/components/UnityFrame/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./UnityFrame.css";
 
 import Unity, { UnityContext } from "react-unity-webgl";
@@ -8,20 +8,40 @@ import { useParams } from "react-router-dom";
 
 
 const unityContext = new UnityContext({
-    loaderUrl: "build/receptor.loader.js",
-    dataUrl: "build/receptor.data",
-    frameworkUrl: "build/receptor.framework.js",
-    codeUrl: "build/receptor.wasm",
+    loaderUrl: "build/max.loader.js",
+    dataUrl: "build/max.data",
+    frameworkUrl: "build/max.framework.js",
+    codeUrl: "build/max.wasm",
   });
 
 
 const UnityFrame = () =>  {
     const {courseID} = useParams();
     const data = useFetchSlidesByCourse(Number(courseID));
+
     if(data !== "Loading..."){
       console.log(JSON.stringify(data));
       unityContext.send("SlideManager", "getData", JSON.stringify(data));
+
     }
+
+    const [video, setVideo] = useState("false");
+    const [progress, setProgress] = useState(0);
+  
+    useEffect(function () {
+      unityContext.on("isVideo", function (video) {
+        setVideo(video);
+        console.log(video);
+      });
+    }, []);
+
+    useEffect(function () {
+        unityContext.on("Close", function (progress) {
+            setProgress(progress);
+            console.log(progress)
+        });
+    }, []);
+
     //console.log("UnityWindow: "+window.localStorage.getItem("loggedUserID"));
     return( 
         <>
